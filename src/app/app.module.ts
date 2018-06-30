5e0b09c7b44d76f12ebcaa5eeec364e0b78d474a
Viviane Takeda <vivi.takeda@gmail.com>
2018-06-30 01:37:11 -0300

    nova atualizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
